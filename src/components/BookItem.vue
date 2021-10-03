9b7f9bee3b2086abfdb840cc3e8fdc031ef6f6d1
LAPTOP-20Q48UMK\User <624235032@parichat.skru.ac.th>
2021-10-03 17:08:05 +0700

    add lab bok

---
+++ src/components/BookItem.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="book-item"> 
+        <h1>{{book.title}}</h1><br/>
+        <img v-bind:src="book.thumbnailUrl" /><br/>
+        <b>Author:</b> {{book.author}} <br/>
+        <b>Category:</b> {{book.category}} <br/>
+        <b>ISBN:</b> {{book.isbn}} <br/>
+        <b>Pages:</b> {{book.pageCount}} <br/>
+        <b>Description:</b><br/>
+        <p>{{book.shortDescription}}</p> 
+        
+
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'BookItem',
+  components: {
+
+  },
+  props:["book"]
+
+
+}
+</script>
+
+
+
+<style>
+.book-item{
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+}
+
+</style>
